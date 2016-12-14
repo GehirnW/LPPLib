41102c1ef589bea5671f3d8413839ad6cdb6a3ee
iLampard <siqiao_xue@163.com>
2016-12-14 09:58:45 +0800

    Updated utils functions(move helper class to calib.h)

--- utils.h
+++ utils.h
@@ -1,33 +1,10 @@
 #include <ql/quantlib.hpp>
-#include <string>
 #include <fstream>
 #include <iostream>
 
 using namespace QuantLib;
 
-
-//! helper class handling data for calibration 
-class LPPLHelper
-{
-public:
-	LPPLHelper(string strFilePath, Integer iColTimeIndex = 0):
-	  strFilePath_(strFilePath), iColTimeIndex_(iColTimeIndex)
-	{
-		QL_REQUIRE(iColTimeIndex >= 0, "LPPLHelper: Date time col index must be a non-negative integer");
-	}
-private:
-	string strFilePath_;
-	std::vector<std::vector<Real> > dataMatrix_;
-	std::vector<std::string > datetime_;  
-	Integer iColTimeIndex_; // col index for date time
-};
-
-
-
-
-
-
-/********************************************
+/*
 // read matrix data from txt file
 // return vector<vector>, each col in txt => row vector 
 void readMatrixFromTxt(string strPath, int iColSize)
